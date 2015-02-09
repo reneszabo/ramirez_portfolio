33c07ec851b5eaa31979a1b0574626446a994d2f
Rene Ramirez <rene.ramirez@verlas.com.ve>
2015-02-09 12:32:06 -0500

    Show post DESC

--- src/Main/Page/BlogBundle/Controller/DefaultController.php
+++ src/Main/Page/BlogBundle/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends Controller {
 
   public function indexAction() {
     $postRepository = $this->getDoctrine()->getRepository('MainEntityBundle:Post');
-    $posts = $postRepository->findAll();
+    $posts = $postRepository->findBy(array(), array('createdAt'=>'DESC'));
     return $this->render('MainPageBlogBundle:Default:index.html.twig', array('posts' => $posts));
   }
 
