e88a807f974df163edb5ead18cfddcdadf3c5e8b
Rene Ramirez <rene.ramirez@4pixels.co>
2016-03-05 14:37:20 -0500

    fix css path

--- src/Main/Page/BackendBundle/Controller/DefaultController.php
+++ src/Main/Page/BackendBundle/Controller/DefaultController.php
@@ -4,13 +4,14 @@ namespace Main\Page\BackendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Main\EntityBundle\Entity\Comment;
+
 class DefaultController extends Controller {
 
   public function indexAction() {
     $postRepository = $this->getDoctrine()->getRepository('MainEntityBundle:Post');
     $commentRepository = $this->getDoctrine()->getRepository('MainEntityBundle:Comment');
     $posts = $postRepository->findAll();
-    $comments = $commentRepository->findBy(array(), array('createdAt'=>'ASC'), 10);
+    $comments = $commentRepository->findBy(array(), array('createdAt' => 'ASC'), 10);
     return $this->render('MainPageBackendBundle:Default:index.html.twig', array('posts' => $posts, 'comments' => $comments));
   }
 
